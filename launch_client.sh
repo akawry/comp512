a1b4cfffd76909b39c6267476a64b087d29524d3
Matthieu Dubet <maattdd@gmail.com>
2011-11-05 23:59:00 -0400

    bugfix for new client param, should end up with a better solution soon though..

--- launch_client.sh
+++ launch_client.sh
@@ -1,20 +1,20 @@
 #!/bin/bash
 
 function print_usage() {
-	echo "Usage: $0 rmi/tcp host:port"
+	echo "Usage: $0 rmi/tcp host:port auto_params"
 	exit 1
 }
 
-if [ $# -ne 2 ] ; then
+if [ $# -ne 3 ] ; then
 	print_usage ;
 fi
 
 cd bin/
 case "$1" in 
 	"tcp" )
-		java Client.TCPClient $2 ;;
+		java Client.TCPClient $2 $3;;
 	"rmi" )
-		java -Djava.security.policy=client.policy Client.RMIClient $2 ;;
+		java -Djava.security.policy=client.policy Client.RMIClient $2 $3;;
 	*)
-		print_usage 
+		print_usage
 esac
--- launch_localhost.sh
+++ launch_localhost.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 
 if [ $# != 6 -a $# != 5 ] ; then
-	echo "Usage : $0 rmi/tcp portcar portflight portroom portmiddleware [input]"
+	echo "Usage : $0 rmi/tcp portcar portflight portroom portmiddleware automatic_client [input]"
 	exit 1
 fi	
 
@@ -14,8 +14,8 @@ sleep 1
 ( ./launch.sh $1 middleware $5 -car=localhost:$2 -flight=localhost:$3 -room=localhost:$4 )
 sleep 1
 if [ $# = 6 ] ; then
-	( ./launch_client.sh $1 localhost:$5 < $6 )
+	( ./launch_client.sh $1 localhost:$5 false:0:0:0 < $7 )
 else 
-	( ./launch_client.sh $1 localhost:$5 )
+	( ./launch_client.sh $1 localhost:$5 false:0:0:0  )
 fi	
 
